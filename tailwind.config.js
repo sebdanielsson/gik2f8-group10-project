6eefd379948011f301031937e34c3a07897908ea
sebastian <sebastian.danielsson@protonmail.com>
2023-01-10 12:18:28 +0100

    Add more viewport widths for really large windows

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     darkMode: "class", // 'media' or 'class'
     content: [join(__dirname, "src/**/*.{html,js,ts}")],
     theme: {
-        extend: {},
+        extend: {
+            screens: {
+                1680: "1680px",
+                1920: "1920px",
+            },
+        },
     },
     plugins: [],
 };
