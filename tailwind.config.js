9efbd7ad39f35d9a2745f28026c4ef57f439d2ca
Sebastian <sebastian.danielsson@proton.me>
2023-03-29 01:04:19 +0200

    Switch to ES modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-const { join } = require("path");
-module.exports = {
-    darkMode: "class", // 'media' or 'class'
-    content: [join(__dirname, "src/**/*.{html,js,ts}")],
-    theme: {
-        extend: {
-            screens: {
-                1680: "1680px",
-                1920: "1920px",
-            },
-        },
+export default {
+  darkMode: "class",
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens: {
+        1680: "1680px",
+        1920: "1920px",
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
